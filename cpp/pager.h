d156999ba8a75071d4a6af9538242878da46f478
snowman8765 <snowman8765@gmail.com>
2016-06-20 06:36:54 +0000

    add cpp/pager.

--- cpp/pager.h
+++ cpp/pager.h
@@ -1,49 +1,45 @@
 #include <stdio.h>
 #include "../c/math.h"
 
-struct pager {
-	int line; // 1ページの行数
-	int num; // 全データ数
-	int currentPage; // 現在のページ番号
-	int lastPage; // 最後のページ番号
-	int startIndex; // 現在のページの開始インデックス
-	int endIndex; // 現在のページの終了インデックス(最後のページの時、[lastPage * line]となる。)
-	int lastIndex; // 現在のページの有効な終了インデックス(最後のページの時、[num - 1]となる。)
+class pager {
+  public:
+    int line; // 1ページの行数
+    int num; // 全データ数
+    int currentPage; // 現在のページ番号
+    int lastPage; // 最後のページ番号
+    int startIndex; // 現在のページの開始インデックス
+    int endIndex; // 現在のページの終了インデックス(最後のページの時、[lastPage * line]となる。)
+    int lastIndex; // 現在のページの有効な終了インデックス(最後のページの時、[num - 1]となる。)
+
+    pager(int num, int line) {
+      this->num = max(num, 0);
+      this->line = line > 0 ? line : 5;
+      this->calc(1);
+    }
+
+    void calc(int currentPage) {
+      if (this->num <= 0) {
+      this->num = 0;
+        this->currentPage = 1;
+        this->lastPage = 1;
+        this->startIndex = 0;
+        this->endIndex = -1;
+        this->lastIndex = -1;
+        return;
+      }
+      this->currentPage = max(currentPage, 1);
+      this->lastPage = (this->num - 1) / this->line + 1;
+      this->currentPage = min(this->currentPage, this->lastPage);
+      this->startIndex = this->line * (this->currentPage - 1);
+      this->endIndex = (this->line * this->currentPage) - 1;
+      this->lastIndex = min(this->endIndex, (this->num - 1));
+    }
+
+    void next() {
+      this->calc(this->currentPage + 1);
+    }
+
+    void prev() {
+      this->calc(this->currentPage - 1);
+    }
 };
-
-inline void initPager(pager* p, int num = 1, int line = 5);
-inline void calc(pager* p, int currentPage = 1);
-inline void next(pager* p);
-inline void prev(pager* p);
-
-inline void initPager(pager* p, int num, int line) {
-	p->num = max(num, 0);
-	p->line = line > 0 ? line : 5;
-	calc(p);
-}
-
-inline void calc(pager* p, int currentPage) {
-	if (p->num <= 0) {
-		p->num = 0;
-		p->currentPage = 1;
-		p->lastPage = 1;
-		p->startIndex = 0;
-		p->endIndex = -1;
-		p->lastIndex = -1;
-		return;
-	}
-	p->currentPage = max(currentPage, 1);
-	p->lastPage = (p->num - 1) / p->line + 1;
-	p->currentPage = min(p->currentPage, p->lastPage);
-	p->startIndex = p->line * (p->currentPage - 1);
-	p->endIndex = (p->line * p->currentPage) - 1;
-	p->lastIndex = min(p->endIndex, (p->num - 1));
-}
-
-inline void next(pager* p) {
-	calc(p, p->currentPage + 1);
-}
-
-inline void prev(pager* p) {
-	calc(p, p->currentPage - 1);
-}
--- cpp/test_pager.c
+++ cpp/test_pager.c
@@ -1,9 +1,45 @@
 #include <stdio.h>
+#include <assert.h>
 #include "pager.h"
 
 int main ( int argc, char *argv[] )
 {
-  pager p = {0};
+  pager p = pager(10, 7);
+
+  assert(p.line == 7);
+  assert(p.num == 10);
+  assert(p.currentPage == 1);
+  assert(p.lastPage == 2);
+  assert(p.startIndex == 0);
+  assert(p.endIndex == 6);
+  assert(p.lastIndex == 6);
+
+  p.prev();
+  assert(p.line == 7);
+  assert(p.num == 10);
+  assert(p.currentPage == 1);
+  assert(p.lastPage == 2);
+  assert(p.startIndex == 0);
+  assert(p.endIndex == 6);
+  assert(p.lastIndex == 6);
+
+  p.next();
+  assert(p.line == 7);
+  assert(p.num == 10);
+  assert(p.currentPage == 2);
+  assert(p.lastPage == 2);
+  assert(p.startIndex == 7);
+  assert(p.endIndex == 13);
+  assert(p.lastIndex == 9);
+
+  p.next();
+  assert(p.line == 7);
+  assert(p.num == 10);
+  assert(p.currentPage == 2);
+  assert(p.lastPage == 2);
+  assert(p.startIndex == 7);
+  assert(p.endIndex == 13);
+  assert(p.lastIndex == 9);
 
   return 0;
 }
